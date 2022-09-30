ae246bea4d52c24d487c55e2c809ec338c193994
jaque20050 <jaque20050@hotmail.com>
2022-09-30 14:13:52 -0300

    Visualizando os Banco de Dados, salvando contatos

--- src/br/com/agenda/aplicacao/Main.java
+++ src/br/com/agenda/aplicacao/Main.java
@@ -11,11 +11,17 @@ public class Main {
 		ContatoDAO contatoDAO = new ContatoDAO();
 
 		Contato contato = new Contato();
-		contato.setNome("Rodrigo");
-		contato.setIdade(27);
+		contato.setNome("Eni Luciane");
+		contato.setIdade(45);
 		contato.setDataCadastro(new Date());
 
 		contatoDAO.save(contato);
+		
+		// Visualização dos registros do banco de dados
+		
+		for(Contato c : contatoDAO.getContatos()) {
+			System.out.println("Contato: " + c.getNome());
+		}
 	}
 
 }
